update depth visualization. correct a bug on pixel value normalization

depthAnything.py
@@ -4,6 +4,10 @@ import numpy as np
 import matplotlib
 import cv2
 
+import imgviz
+
+
+
 from submodules.DepthAnythingV2.depth_anything_v2.dpt import DepthAnythingV2
 
 
@@ -52,7 +56,17 @@ if __name__ == "__main__":
     raw_img = cv2.imread(image_dir)
 
     depth = DA.eval(raw_img) # HxW raw depth map in numpy
-    depth = DA.depth2image(depth, colormap='nipy_spectral')
+    # depth = DA.depth2image(depth, colormap='nipy_spectral')
+
+    depth = ( depth / np.median(depth) ) * 2.0 
+
+
+    depth = imgviz.depth2rgb(depth, min_value=0.3, max_value=5.0)
+    depth = torch.from_numpy(depth)
+    depth = torch.permute(depth, (2, 0, 1)).float()
+    depth = (depth).byte().permute(1, 2, 0).contiguous().cpu().numpy()
+    # rgb = o3d.geometry.Image(depth)
+
 
     cv2.imshow('raw-image', raw_img)
     cv2.imshow('depth-image', depth)

gui/sfm_gui.py
@@ -499,13 +499,14 @@ class SFM_GUI:
         if gaussian_packet.gtdepth is not None:
             depth = gaussian_packet.gtdepth
             depth = imgviz.depth2rgb(
-                depth, min_value=0.1, max_value=5.0, colormap="jet"
+                depth, min_value=0.1, max_value=5.0, colormap="turbo"
             )
             depth = torch.from_numpy(depth)
             depth = torch.permute(depth, (2, 0, 1)).float()
             depth = (depth).byte().permute(1, 2, 0).contiguous().cpu().numpy()
             rgb = o3d.geometry.Image(depth)
             self.in_depth_widget.update_image(rgb)
+
 
         if gaussian_packet.finish:
             Log("Received terminate signal", tag="GUI")

sfm.py
@@ -394,14 +394,13 @@ class SFM(mp.Process):
                 if self.use_gui and (iteration % 10 == 0 or calib_profiling):
                     # depth = np.zeros((self.viewpoint_stack[0].image_height, self.viewpoint_stack[0].image_width))
                     cam_cnt = (cam_cnt+1) % len(self.viewpoint_stack)
-                    img = self.viewpoint_stack[cam_cnt].original_image.cpu().squeeze(0).numpy()
-                    # convert (3, H, W)  --- > (H, W, 3)
-                    # coorect color order BGR2RGB
-                    cv_img = img.transpose(1, 2, 0)
-                    cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+                    # img = (self.viewpoint_stack[cam_cnt].original_image*255).cpu().squeeze(0).numpy()
+                    # cv_img = img.transpose(1, 2, 0)
+                    # cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+                    cv_img = (self.viewpoint_stack[cam_cnt].original_image*255).byte().permute(1, 2, 0).contiguous().cpu().numpy()
                     # use depth prediction from a Neural network                    
-                    depth = self.depth_anything.eval(cv_img) / self.depth_scale
-                    depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+                    depth = self.depth_anything.eval(cv_img)
+                    depth = ( depth / np.median(depth) ) * 2.5
 
                     self.q_main2vis.put(
                         gui_utils.GaussianPacket(

test_colmap_sfm_routine.py
@@ -11,6 +11,8 @@ import pycolmap
 from gaussian_splatting.utils.graphics_utils import BasicPointCloud
 
 from sfm import SFM
+from sfm import DepthAnything
+
 from gaussian_splatting.scene.gaussian_model_GS import GaussianModel
 from gaussian_splatting.scene.cameras import Camera
 from gui import gui_utils, sfm_gui
@@ -25,7 +27,7 @@ from utils.multiprocessing_utils import FakeQueue, clone_obj
 from PIL import Image
 from gaussian_splatting.utils.general_utils import PILtoTorch
 
-
+import open3d as o3d
 
 # pip install pycolmap
 class ColMap:
@@ -233,6 +235,7 @@ if __name__ == "__main__":
 
 
 
+    use_colmap_point_cloud = True
 
 
     image_dir = "/home/fang/SURGAR/Colmap_Test/Fountain/images"
@@ -251,8 +254,24 @@ if __name__ == "__main__":
     print(f"scale_info = {scale_info}")
     cameras_extent = scale_info["radius"]
     gaussians = GaussianModel(sh_degree=0)
-    gaussians.create_from_pcd(pcd, cameras_extent)
-    # create_pcd_from_image_and_depth(self, cam, rgb, depth, init=False)
+
+
+    
+    if use_colmap_point_cloud:
+        gaussians.create_from_pcd(pcd, cameras_extent)
+
+    else:
+        cam = viewpoint_stack[0]
+
+        rgb_raw = (cam.original_image *255).byte().permute(1, 2, 0).contiguous().cpu().numpy()
+        # use depth prediction from a Neural network
+        depth_raw = DepthAnything().eval(rgb_raw)
+
+        rgb = o3d.geometry.Image(rgb_raw.astype(np.uint8))
+        depth = o3d.geometry.Image(depth_raw.astype(np.float32))
+
+        gaussians.create_from_image_and_depth(cam, rgb, depth, downsample_factor = 8, point_size = 0.01)
+
 
 
 
@@ -284,7 +303,7 @@ if __name__ == "__main__":
 
     sfm = SFM(pipe, q_main2vis, q_vis2main, use_gui, viewpoint_stack, gaussians, opt, cameras_extent)
     sfm.add_calib_noise_iter = -1
-    sfm.start_calib_iter = 150
+    sfm.start_calib_iter = 250
     sfm.require_calibration = True
     sfm.allow_lens_distortion = True
     