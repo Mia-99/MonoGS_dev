735fcf747c8275b695e4fd7206968b0165249d9f
FangBai-Surgar <fang.bai@surgar-surgery.com>
2024-09-19 18:47:17 +0200

    update frontend

--- utils/slam_frontend.py
+++ utils/slam_frontend.py
@@ -388,7 +388,7 @@ class FrontEnd(mp.Process):
 
                 # copy the last calibration to current viewpoint
                 if len(self.current_window):
-                    last_frame_idx = self.current_window[-1]
+                    last_frame_idx = self.current_window[0]
                     fx = self.cameras[last_frame_idx].fx
                     fy = self.cameras[last_frame_idx].fy
                     kappa = self.cameras[last_frame_idx].kappa
