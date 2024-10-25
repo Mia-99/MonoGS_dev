ade0ae3509a4a967684b705223aca96b64f083d2
FangBai-Surgar <fang.bai@surgar-surgery.com>
2024-10-25 13:26:44 +0200

    save

--- utils/slam_backend.py
+++ utils/slam_backend.py
@@ -151,7 +151,7 @@ class BackEnd(mp.Process):
         Log("Initialized map")
         return render_pkg
 
-    def map(self, current_window, prune=False, calibrate=False, fix_gaussian = False, iters=1, force_prune = False):
+    def map(self, current_window, prune=False, calibrate=False, fix_gaussian = False, iters=1, force_densify_prune = False):
         if len(current_window) == 0:
             return
 
@@ -248,7 +248,6 @@ class BackEnd(mp.Process):
                     kf_idx = current_window[idx]
                     n_touched = n_touched_acm[idx]
                     self.occ_aware_visibility[kf_idx] = (n_touched > 0).long()
-
                 # # compute the visibility of the gaussians
                 # # Only prune on the last iteration and when we have full window
                 if prune and (not fix_gaussian):
@@ -293,12 +292,11 @@ class BackEnd(mp.Process):
                     self.gaussians.add_densification_stats(
                         viewspace_point_tensor_acm[idx], visibility_filter_acm[idx]
                     )
-
                 update_gaussian = (
                     self.iteration_count % self.gaussian_update_every
                     == self.gaussian_update_offset
                 )
-                if ( update_gaussian and (not fix_gaussian) ) or force_prune:
+                if ( update_gaussian and (not fix_gaussian) ) or force_densify_prune:
                     self.gaussians.densify_and_prune(
                         self.opt_params.densify_grad_threshold,
                         self.gaussian_th,
@@ -445,12 +443,13 @@ class BackEnd(mp.Process):
                     self.push_to_frontend("init")
 
                 elif data[0] == "calibration_change":
-                    self.map(self.current_window, prune=True, calibrate=False, iters=10) # to perform excessive pruning
-                    self.map(self.current_window, prune=True, calibrate=False, iters=1, force_prune=True) # prune. force densification and split
-                    self.map(self.current_window, prune=False, calibrate=False, iters=10) # optimize Gaussian parameters only
-                    self.map(self.current_window, prune=True, calibrate=False, iters=1) # prune
-                    self.push_to_frontend()
                     rich.print("[bold red]Backend : calibration change signal recieved [/bold red]")
+                    self.map(self.current_window, prune=True, iters=10) # to perform excessive pruning
+                    # self.map(self.current_window, prune=False, iters=1, force_densify_prune=True) # force densification and split
+                    self.map(self.current_window, prune=False, iters=10) # optimize Gaussian parameters only
+                    self.map(self.current_window, prune=True, iters=1) # prune
+                    self.push_to_frontend()
+                    rich.print("[bold red]Backend : Gaussians optimized for calibration [/bold red]")
 
                 elif data[0] == "keyframe":
                     cur_frame_idx = data[1]
@@ -603,7 +602,7 @@ class BackEnd(mp.Process):
                     if (self.signal_calibration_change):
                         self.add_next_kf(cur_frame_idx, self.viewpoints[cur_frame_idx], depth_map=depth_map) 
                         self.map(self.current_window, calibrate=False, iters=iter_per_kf) # don't calibrate with one view. optimize gaussian
-                        self.map(self.current_window, prune=True)
+                        # self.map(self.current_window, prune=True)
 
 
                 else:
