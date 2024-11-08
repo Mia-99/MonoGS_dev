afd651409cfb965e8b5edacb76dfa35ac0924b0f
Mia-99 <329208364@qq.com>
2024-11-09 00:08:24 +0200

    add evaluation in sfm

--- run_colmap_sfm_pipeline.py
+++ run_colmap_sfm_pipeline.py
@@ -1,6 +1,9 @@
 
+import time
+from datetime import datetime
 import sys, os
 
+from gaussian_splatting.gaussian_renderer import render
 import torch
 import torch.multiprocessing as mp
 
@@ -31,8 +34,12 @@ from colmap import ColMap
 from colmap import assemble_3DGS_cameras
 
 from gaussian_viewer import Viewer, create_gaussians_gl
-
-
+from utils_cali.eval_cali_utils import eval_ate, save_cali, save_gaussians_class, eval_rendering
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
+from gaussian_splatting.utils.image_utils import psnr
+from gaussian_splatting.utils.loss_utils import ssim
+import cv2
+import json
 # from depth_anything import DepthAnything
 # def init_dense_pcd_from_network (viewpoint_stack, reconstruction: ColMap, num_points = 20000):
 
@@ -147,7 +154,7 @@ if __name__ == "__main__":
     '''
     
     data_url = "https://cvg-data.inf.ethz.ch/local-feature-evaluation-schoenberger2017/Strecha-Fountain.zip"
-    image_dir = "/hdd/sfm/Strecha-Fountain/Fountain/images"
+    image_dir = "/datasets/Strecha-Herzjesu/Herzjesu/images"
     '''
     ground_truth calibration:
         2759.48 0 1520.69
@@ -187,9 +194,143 @@ if __name__ == "__main__":
     use_gui = True
     sfm = SFM(pipe, use_gui, viewpoint_stack, gaussians, opt, cameras_extent)
     sfm.optimize()
-    sfm.close()
+    # sfm.close()
+    
+    current_datetime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    path = "./results/sfm/" + image_dir.split("/")[-2] + "/" + current_datetime
+    # read R_gt, t_gt
+    # image_dir
+    # R_gt, t_gt = read_gt(image_dir)
 
-    sfm.show_rendered_images()
+    def load_gt(image_directory):
+        print(f"Loading ground truth camera parameters from {image_directory}")
+        # remove the '/images' in the directory
+        parent_directory = os.path.dirname(image_directory)
+        groundtruth_directory = os.path.join(parent_directory, 'groundtruth')
+        camera_files = [f for f in os.listdir(groundtruth_directory) if f.endswith('.camera')]
+        print(f"camera_files = {camera_files}")
+        camera_files.sort()  # Ensure numerical order
+
+        all_camera_params = []  # List to store all camera parameters
+        fxs = []
+        fys = []
+        R_gts = []
+        T_gts = []
+
+        for filename in camera_files:
+            filepath = os.path.join(groundtruth_directory, filename)
+            with open(filepath, 'r') as file:
+                lines = file.readlines()
+
+                # Parsing intrinsic matrix
+                intrinsic = np.array([list(map(float, lines[i].strip().split())) for i in range(3)])
+                fx = intrinsic[0, 0]
+                fxs.append(fx)
+                fy = intrinsic[1, 1]
+                fys.append(fy)
+
+                # Parsing extrinsic parameters (rotation matrix and translation vector)
+                rotation = np.array([list(map(float, lines[i].strip().split())) for i in range(4, 7)])
+                R_gts.append(torch.tensor(rotation, dtype=torch.float32, device=torch.device('cuda')))
+                translation = np.array(list(map(float, lines[7].strip().split())))
+                T_gts.append(torch.tensor(translation, dtype=torch.float32, device=torch.device('cuda')))
+                
+                
+                # Image dimensions
+                dimensions = list(map(int, lines[8].strip().split()))
+
+                # Store in a dictionary
+                camera_params = {
+                    'intrinsic': intrinsic,
+                    'rotation': rotation,
+                    'translation': translation,
+                    'dimensions': dimensions
+                }
+                all_camera_params.append(camera_params)
+        
+        return R_gts, T_gts, fxs, fys
+    # load_gt(image_dir)
+    R_gts, T_gts, fxs, fys = load_gt(image_dir)
+    # print( sfm.viewpoint_stack)
+    # print(len(R_gts))
+    for i in range(len(sfm.viewpoint_stack)):
+        sfm.viewpoint_stack[i].R_gt = R_gts[i]
+        sfm.viewpoint_stack[i].T_gt = T_gts[i]
+        sfm.viewpoint_stack[i].fx_init = fxs[i]/downsample_scale
+        sfm.viewpoint_stack[i].fy_init = fys[i]/downsample_scale
+        
+    eval_ate(sfm.viewpoint_stack, [i for i in range(len(sfm.viewpoint_stack))], save_dir=path, iterations=0, final=True, monocular=True)
+    save_cali(save_dir=path, frames=sfm.viewpoint_stack, kf_indices=[i for i in range(len(sfm.viewpoint_stack))], N_frames=None)
+    save_gaussians_class(save_dir=path, gaussians=sfm.gaussians)
+    
+    def save_rendering(viewpoints, gaussians, kf_indices, pipeline_params, save_path):
+        bg_color = [0, 0, 0]
+        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+        dir = os.path.join(save_path, 'rendering')
+        os.makedirs(os.path.join(dir, 'pred'), exist_ok=True)
+        os.makedirs(os.path.join(dir, 'gt'), exist_ok=True)
+        img_pred, img_gt, saved_frame_idx = [], [], []
+        psnr_array, ssim_array, lpips_array = [], [], []
+        cal_lpips = LearnedPerceptualImagePatchSimilarity(
+            net_type="alex", normalize=True
+        ).to("cuda")
+        for i in range(len(kf_indices)):
+            idx = kf_indices[i]
+            # gt_image format:
+            # torch.Size([3, 600, 800])
+            # torch.float32
+            # cuda:0
+            # let viewpoints[idx].original_image in cuda:0 device
+            gt_image = viewpoints[idx].original_image.to("cuda:0")
+            viewpoint = viewpoints[idx]
+            # viewpoint.compute_grad_mask(self.config)
+
+
+            # TODO: add pipeline_params and background
+
+            rendering = render(viewpoint, gaussians, pipeline_params, background)["render"]
+            image = torch.clamp(rendering, 0.0, 1.0)
+            # save image
+            gt = (gt_image.cpu().numpy().transpose((1, 2, 0)) * 255).astype(np.uint8)
+            gt = cv2.cvtColor(gt, cv2.COLOR_BGR2RGB)
+            pred = (image.detach().cpu().numpy().transpose((1, 2, 0)) * 255).astype(np.uint8)
+            pred = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR)
+
+            # print(gt.shape)  # Should show a tuple of (height, width, channels) for color or (height, width) for grayscale
+            # print(gt.dtype)  # Should typically be 'uint8' for image data
+            # print(os.path.join(dir, f'gt/image_gt_{idx}.png'))
+            cv2.imwrite(os.path.join(dir, f'pred/image_pred_{idx}.png'), pred)
+            state = cv2.imwrite(os.path.join(dir, f'gt/image_gt_{idx}.png'), gt)
+
+
+            mask = gt_image > 0
+            psnr_score = psnr((image[mask]).unsqueeze(0), (gt_image[mask]).unsqueeze(0))
+            ssim_score = ssim((image).unsqueeze(0), (gt_image).unsqueeze(0))
+            lpips_score = cal_lpips((image).unsqueeze(0), (gt_image).unsqueeze(0))
+
+            psnr_array.append(psnr_score.item())
+            ssim_array.append(ssim_score.item())
+            lpips_array.append(lpips_score.item())
+            # path/pred/image_pred_{i}.png
+
+        print('mean psnr:', np.mean(psnr_array))
+        print('mean ssim:', np.mean(ssim_array))
+        print('mean lpips:', np.mean(lpips_array))
+        # write mean psnr, ssim, lpips to a file
+        with open(os.path.join(dir, 'psnr_final_result.json'), 'w') as file:
+            json.dump({
+                'mean_psnr': np.mean(psnr_array),
+                'mean_ssim': np.mean(ssim_array),
+                'mean_lpips': np.mean(lpips_array),
+                'psnr_array': psnr_array,
+                'ssim_array': ssim_array,
+                'lpips_array': lpips_array
+            }, file)
+        return rendering
+    
+    save_rendering(sfm.viewpoint_stack, sfm.gaussians, [i for i in range(len(sfm.viewpoint_stack))], pipe, path)
+    # eval_rendering, save_gaussians_class
+    # sfm.show_rendered_images()
     
 
 
