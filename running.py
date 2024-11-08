7435adfc7a208c0f8f018cc03de344f0e500219f
Mia-99 <329208364@qq.com>
2024-11-09 00:14:24 +0200

    update script for experiment running

--- running.py
+++ running.py
@@ -2,13 +2,13 @@ import os
 os.environ['MPLBACKEND'] = 'Agg'
 
 # Base command
-# types=['mono', 'rgbd']
+types=['mono', 'rgbd']
 # seqs = ["0", "1", "2", "3", "4"]
 # versions = ['', '_v0_sp', '_v0', '_v1_sp', '_v1', '_v2_sp', '_v2', '_v3_sp', '_v3', '_v4_sp', '_v4', '_v5_sp', '_v5', '_v6_sp', '_v6', '_v7', '_v8']
 
 
 # types=['mono']
-types=['rgbd']
+# types=['rgbd']
 # seqs = ["0"]
 # versions = ['_v6_sp', '_v6']
 # seqs = ["1"]
@@ -29,11 +29,18 @@ types=['rgbd']
 #     # "4": ["_v0_sp", "_v0"]
 # }
 data_dict = {
-    "0": ["_v6"],
-    "1": ["_v0"],
-    "2": ["_v3"],
-    "3": ["_v9"],
-    "4": ["_v0"]
+    "0": ["_v6",''],
+    "1": ["_v0",''],
+    "2": ["_v3",''],
+    "3": ["_v9",'_v10',''],
+    "4": ["_v0",'']
+}
+data_dict = {
+    "0": ["_sp"],
+    "1": ["_sp"],
+    "2": ["_sp"],
+    "3": ["_sp"],
+    "4": ["_sp"]
 }
 # data_dict = {
 #     # "0": [""],
@@ -62,7 +69,7 @@ for type in types:
                 os.makedirs(f'./cmd_output/office{seq}', exist_ok=True)
                 output_file = f'./cmd_output/office{seq}/office{seq}_{version}.txt'
                 # base_command = f"python slam_cali.py --config {config_file_path} --eval --require_calibration --allow_lens_distortion | tee {output_file}"
-                # base_command = f"python slam_cali.py --config {config_file_path} --eval --require_calibration | tee {output_file}"
+                base_command = f"python slam_cali.py --config {config_file_path} --eval --require_calibration | tee {output_file}"
                 base_command = f"python slam_cali.py --config {config_file_path} --eval"
                 
                 # Construct the full command
