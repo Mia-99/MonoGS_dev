64a92d934d0533b13da93eeaac935008d1ed0382
FangBai-Surgar <fang.bai@surgar-surgery.com>
2025-01-06 18:35:46 +0100

    update backend costs. need furter tests

--- utils/slam_backend.py
+++ utils/slam_backend.py
@@ -392,7 +392,8 @@ class BackEnd(mp.Process):
             self.last_sent += 1
 
             loss_mapping = 0
-            for cam_idx in range(len(current_window)):
+            # for cam_idx in range(len(current_window)):
+            for cam_idx in range(min(frames_to_optimize, len(current_window))):
                 viewpoint = viewpoint_stack[cam_idx]
                 render_pkg = render(
                     viewpoint, self.gaussians, self.pipeline_params, self.background
@@ -414,32 +415,31 @@ class BackEnd(mp.Process):
                     render_pkg["opacity"],
                     render_pkg["n_touched"],
                 )
-                # use tracking loss here
+                # use tracking loss here                
                 loss_mapping += get_loss_tracking(
                                 self.config, image, depth, opacity, viewpoint
-                )
-                # loss_mapping += get_loss_mapping(
+                ) 
+                # if cam_idx in self.calibration_window else get_loss_mapping(
                 #     self.config, image, depth, viewpoint, opacity
                 # )
             loss_mapping.backward()
 
             with torch.no_grad():
 
-                if calibrate and self.require_calibration and self.initialized:
-                    if (self.calibration_optimizers is not None):
+                if (self.calibration_optimizers is not None):
+                    if calibrate and self.require_calibration and self.initialized:
                         self.calibration_optimizers.focal_step()
                         if self.allow_lens_distortion and cur_itr > 5:
                             self.calibration_optimizers.kappa_step()
-                if self.calibration_optimizers is not None:
                     self.calibration_optimizers.zero_grad(set_to_none=True)
                 
-                self.keyframe_optimizers.step()
-                self.keyframe_optimizers.zero_grad(set_to_none=True)
+                self.keyframe_optimizers.step()                
                 for cam_idx in range(min(frames_to_optimize, len(current_window))):
                     viewpoint = viewpoint_stack[cam_idx]
                     if viewpoint.uid == 0:
                         continue
                     update_pose(viewpoint)
+                self.keyframe_optimizers.zero_grad(set_to_none=True)
 
                 self.gaussians.optimizer.zero_grad(set_to_none=True)
         return
@@ -577,8 +577,8 @@ class BackEnd(mp.Process):
                 """
                 if len(self.calibration_window):
                     # if len(self.calibration_window) == 1 only, this allows two-view Gaussian optimization before adding the third view
-                    fix_gaussian= ( len(self.calibration_window) == 1 or len(self.calibration_window) == 2)
-                    # fix_gaussian= ( len(self.calibration_window) == 1 )
+                    # fix_gaussian= ( len(self.calibration_window) == 1 or len(self.calibration_window) == 2)
+                    fix_gaussian= ( len(self.calibration_window) == 1 )
                     self.map(self.current_window, calibrate=len(self.calibration_window), fix_gaussian=fix_gaussian)
                 else:
                     self.map(self.current_window)
@@ -615,15 +615,15 @@ class BackEnd(mp.Process):
 
                 elif data[0] == "calibration_change":
                     rich.print("[bold red]Backend : calibration change signal recieved [/bold red]")                    
-                    self.gaussians.densify_and_prune(
-                        self.opt_params.densify_grad_threshold,
-                        self.gaussian_th,
-                        self.gaussian_extent,
-                        self.size_threshold,
-                    )
-                    self.map(self.current_window, iters=10 )
-                    self.map(self.current_window, prune=True, iters=1)
-                    self.push_to_frontend()
+                    # self.gaussians.densify_and_prune(
+                    #     self.opt_params.densify_grad_threshold,
+                    #     self.gaussian_th,
+                    #     self.gaussian_extent,
+                    #     self.size_threshold,
+                    # )
+                    # self.map(self.current_window, iters=10 )
+                    # self.map(self.current_window, prune=True, iters=1)
+                    # self.push_to_frontend()
                     rich.print("[bold red]Backend : calibration change signal processed [/bold red]")   
 
                 elif data[0] == "keyframe":
